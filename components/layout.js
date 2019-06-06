3007cbb2a3ae1e188ad058eb87bb348d3244cf74
Kylee Tilley <kyleetilley@gmail.com>
2019-06-05 21:57:12 -0500

    Add title & subtitle to layout

--- components/layout.js
+++ components/layout.js
@@ -1,13 +1,25 @@
 import React from "react";
+import Head from "next/head";
+import PropTypes from "prop-types";
 
 export default function Layout(props) {
   const { children } = props;
+  const subTitle = props.title ? props.title : "";
+  const siteTitle = "Example Test App";
 
   return (
     <>
+      <Head>
+        <title>{subTitle ? `${subTitle} - ${siteTitle}` : siteTitle}</title>
+      </Head>
+
       <h1>Title</h1>
 
       {children}
     </>
   );
 }
+
+Layout.propTypes = {
+  title: PropTypes.string
+};
