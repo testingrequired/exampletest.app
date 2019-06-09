afb2d9b3658e72c7c3baeb0dd1e9684859a72d22
Kylee Tilley <kyleetilley@gmail.com>
2019-06-09 12:17:54 -0500

    Update index message

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from "./layout";
 export default function Index() {
   return (
     <Layout title="Home">
-      <p>Share your sour thoughts!</p>
+      <p>Share your thoughts!</p>
     </Layout>
   );
 }
