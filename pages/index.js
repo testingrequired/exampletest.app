527a8c372b23e7010c5f04a1e8178e4a14275dc3
Kylee Tilley <kyleetilley@gmail.com>
2019-06-14 22:31:50 -0500

    Add top and trending posts to home page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
 import Layout from "../layouts/layout";
+import RandomPosts from "../components/randomPosts";
 
 export default function IndexPage() {
   return (
     <Layout title="Home">
-      <p>Share your thoughts!</p>
+      <h2>Top Posts</h2>
+
+      <RandomPosts n={10} minLikes={3000} maxLikes={6000} />
+
+      <h2>Trending Posts</h2>
+
+      <RandomPosts n={25} minLikes={250} maxLikes={1000} />
     </Layout>
   );
 }
