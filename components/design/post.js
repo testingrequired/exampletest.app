abf64b8e926926c69f40af61574fccc556723468
Kylee Tilley <kyleetilley@gmail.com>
2019-07-08 19:42:26 -0500

    Link fix

--- components/design/post.js
+++ components/design/post.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Post({ username, body, likes }) {
   return (
     <p>
-      <Link href={`users/${username}`}>
+      <Link href={`users/[username]`} as={`users/${username}`}>
         <a>@{username}</a>
       </Link>
       : {body} ({likes} likes)
