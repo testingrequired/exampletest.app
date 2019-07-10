dd1011c28878bc50ecfbcca9fd62e279dc584eae
Kylee Tilley <kyleetilley@gmail.com>
2019-07-10 18:29:12 -0500

    Restyle posts

---
+++ components/design/likeLink.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export default styled.button`
+  border: 0;
+  background: none;
+  cursor: pointer;
+  font-size: 1em;
+
+  &:active {
+    font-weight: bold;
+    outline: 0;
+  }
+`;
--- components/design/post.js
+++ components/design/post.js
@@ -1,26 +1,56 @@
 import { useState } from "react";
-
 import UserLink from "../userLink";
+import LikeLinkButton from "./likeLink";
+import styled from "styled-components";
+
+const PostStyle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 25px 0;
+
+  .user {
+    width: 10%;
+
+    @media (max-width: 1000px) {
+      width: 25%;
+    }
+  }
+
+  .body {
+    width: 70%;
+
+    @media (max-width: 1000px) {
+      width: 50%;
+    }
+  }
+
+  .likes {
+    width: 15%;
+  }
+`;
 
 export default function Post({ username, body, likes }) {
   const [postLikes, setPostLikes] = useState(likes);
 
   return (
-    <p>
-      <UserLink username={username}>@{username}</UserLink> {body} ({postLikes}{" "}
-      likes) <LikeLink />
-    </p>
+    <PostStyle>
+      <section className="user">
+        <UserLink username={username}>@{username}</UserLink>
+      </section>
+      <section className="body">{body}</section>
+      <section className="likes">
+        <LikeLink />
+      </section>
+    </PostStyle>
   );
 
   function LikeLink() {
-    return postLikes === likes ? (
-      <a href="" onClick={onClickLike}>
-        Like
-      </a>
-    ) : (
-      <a href="" onClick={onClickUnlike}>
-        Unlike
-      </a>
+    return (
+      <LikeLinkButton
+        onClick={postLikes === likes ? onClickLike : onClickUnlike}
+      >
+        {postLikes} Likes
+      </LikeLinkButton>
     );
   }
 
