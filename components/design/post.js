08e2f6fc7dda206e70e02fe026fcc8a908ba9a10
Kylee Tilley <kyleetilley@gmail.com>
2019-07-10 13:19:49 -0500

    Add like link to posts

--- components/design/post.js
+++ components/design/post.js
@@ -1,12 +1,38 @@
+import { useState } from "react";
+
 import UserLink from "../userLink";
 
 export default function Post({ username, body, likes }) {
+  const [postLikes, setPostLikes] = useState(likes);
+
   return (
     <p>
       <UserLink username={username}>
         <a>@{username}</a>
       </UserLink>{" "}
-      {body} ({likes} likes)
+      {body} ({postLikes} likes) <LikeLink />
     </p>
   );
+
+  function LikeLink() {
+    return postLikes === likes ? (
+      <a href="" onClick={onClickLike}>
+        Like
+      </a>
+    ) : (
+      <a href="" onClick={onClickUnlike}>
+        Unlike
+      </a>
+    );
+  }
+
+  function onClickLike(e) {
+    e.preventDefault();
+    setPostLikes(postLikes + 1);
+  }
+
+  function onClickUnlike(e) {
+    e.preventDefault();
+    setPostLikes(postLikes - 1);
+  }
 }
