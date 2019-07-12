19b5d58a995193b5de8dad8d654d60a44b89341a
Kylee Tilley <kyleetilley@gmail.com>
2019-07-11 22:56:01 -0500

    Remove debugger

--- components/design/postList.js
+++ components/design/postList.js
@@ -1,7 +1,6 @@
 import Post from "./post";
 
 export default function PostList({ posts = [], n }) {
-  debugger;
   return posts
     .slice(0, n || posts.length)
     .map((post, i) => <Post key={i} {...post} />);
